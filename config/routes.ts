2796a2a97e1eda4a936c102c8ab6301094b5dedc
Sophus David <95245426@qq.com>
2023-05-13 11:48:48 +0800

    update(UI): remove unused routes :tada:

--- config/routes.ts
+++ config/routes.ts
@@ -28,29 +28,29 @@ export default [
     icon: 'HomeOutlined',
     component: './Welcome',
   },
-  {
-    path: '/admin',
-    name: 'admin',
-    icon: 'crown',
-    access: 'canAdmin',
-    routes: [
-      {
-        path: '/admin',
-        redirect: '/admin/sub-page',
-      },
-      {
-        path: '/admin/sub-page',
-        name: 'sub-page',
-        component: './Admin',
-      },
-    ],
-  },
-  {
-    name: 'list.table-list',
-    icon: 'table',
-    path: '/list',
-    component: './TableList',
-  },
+  // {
+  //   path: '/admin',
+  //   name: 'admin',
+  //   icon: 'crown',
+  //   access: 'canAdmin',
+  //   routes: [
+  //     {
+  //       path: '/admin',
+  //       redirect: '/admin/sub-page',
+  //     },
+  //     {
+  //       path: '/admin/sub-page',
+  //       name: 'sub-page',
+  //       component: './Admin',
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: 'list.table-list',
+  //   icon: 'table',
+  //   path: '/list',
+  //   component: './TableList',
+  // },
   {
     path: '/',
     redirect: '/workspace',
@@ -71,12 +71,11 @@ export default [
   },
   {
     path: '/show_model',
-    
+
     component: './ShowModel/ShowModel',
   },
   {
     path: 'test',
-    component: './Test/test'
-  }
-  
+    component: './Test/test',
+  },
 ];
